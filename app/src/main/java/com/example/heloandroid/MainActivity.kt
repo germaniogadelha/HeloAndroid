e5fe60e7cc48c829224013c26504723d77827c86
germa <germaniogadelha2011@hotmail.com>
2023-05-07 16:27:46 -0300

    alterção teste 1

--- app/src/main/java/com/example/heloandroid/MainActivity.kt
+++ app/src/main/java/com/example/heloandroid/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.heloandroid.ui.theme.HeloAndroidTheme
+// teste
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
